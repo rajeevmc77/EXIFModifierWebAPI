7758ab3250aaf9ac5be19bc57c6e2f3601f2c3d1
Rajeev <rajeevmc77@gmail.com>
2019-09-17 14:31:14 +0530

    added all dependencies

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var express = require('express');
-var path = require('path');
+//var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var getJpegForSchool = require('./routes/getJpegForSchool');
 
-var uploadImageFromSchool = require('./routes/uploadImageFromSchool'); 
+var uploadImageFromSchool = require('./routes/uploadImageFromSchool');
 
 var app = express();
+
+app.use(cors({ origin: '*' }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
 // app.use('/api/v1/users', users);
-app.use('/api/getJpegForSchool',getJpegForSchool);
-app.use('/api/uploadImageFromSchool',uploadImageFromSchool);
+app.use('/api/getJpegForSchool', getJpegForSchool);
+app.use('/api/uploadImageFromSchool', uploadImageFromSchool);
 
 module.exports = app;
